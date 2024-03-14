7ea9ec8a6678287691c57cc58d76dcf157fb9e0d
dongyuanjushi <kai.mei@rutgers.edu>
2024-03-14 18:59:22 -0400

    feat: initialize memory modules

---
+++ src/memory/base.py
@@ -0,0 +1,12 @@
+class BaseMemory:
+    def __init__(self):
+        self.memory_pool = {}
+
+    def mem_save(self, agent_id, content):
+        pass
+
+    def mem_load(self, agent_id):
+        pass
+
+    def mem_alloc(self, agent_id):
+        pass
--- src/memory/single_memory.py
+++ src/memory/single_memory.py
@@ -2,10 +2,16 @@ from src.memory.base import BaseMemory
 
 class SingleMemory(BaseMemory):
     def __init__(self):
-        self.memory_pool = []
+        self.memory_pool = {}
 
-    def save_memory(self, content):
-        self.memory_pool.append(content)
+    def mem_save(self, agent_id, content):
+        self.memory_pool[agent_id].append(content)
 
-    def load(self):
-        return self.memory_pool
+    def mem_load(self, agent_id):
+        return self.memory_pool[agent_id]
+    
+    def mem_alloc(self, agent_id):
+        return NotImplementedError
+    
+    def mem_clear(self, agent_id):
+        self.memory_pool[agent_id] = []
