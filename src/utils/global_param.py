bf18d2383fe9f4e8563de8ff95dfc3e8eab686fa
dongyuanjushi <kai.mei@rutgers.edu>
2024-03-14 18:36:16 -0400

    feat: modify global parameters

--- src/utils/global_param.py
+++ src/utils/global_param.py
@@ -7,11 +7,7 @@ from src.agents.agent_process import (
     AgentProcessQueue
 )
 
-global thread_pool
-thread_pool = ThreadPoolExecutor(max_workers=64)
-
-global agent_process_queue
-agent_process_queue = AgentProcessQueue()
+# agent_process_queue = AgentProcessQueue()
 
 global MAX_AID 
 MAX_AID = 256
@@ -25,5 +21,6 @@ agent_pool = []
 global agent_table
 agent_table = {
     "MathAgent": "src.agents.math_agent",
-    "NarrativeAgent": "src.agents.narrative_agent"
+    "NarrativeAgent": "src.agents.narrative_agent",
+    "RecAgent": "src.agents.rec_agent"
 }
