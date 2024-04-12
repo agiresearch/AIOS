d77d43d577dea1b3a871c1eef917c9edc507f511
dongyuanjushi <kai.mei@rutgers.edu>
2024-04-12 15:32:12 -0400

    test: add test for open-sourced llm

--- tests/test_llms.py
+++ tests/test_llms.py
@@ -4,10 +4,43 @@ import os
 
 from src.llms.llms import LLMKernel
 
+from src.agents.agent_process import AgentProcess
+
+from src.context.simple_context import SimpleContextManager
+
 def test_closed_llm():
+    if "GEMINI_API_KEY" not in os.environ or not os.environ["RAPID_API_KEY"]:
+        with pytest.raises(ValueError):
+            llm_type = "gemini-pro"
+            llm = LLMKernel(llm_type)
     llm_type = "gemini-pro"
     llm = LLMKernel(llm_type)
-    prompt = "Craft a tale about a valiant warrior on a quest to uncover priceless treasures hidden within a mystical island."
-    response = llm.address_request(prompt)
-    assert isinstance(response, str)
-    print(response)
+    agent_process = AgentProcess(
+        agent_name = "Narrative Agent",
+        prompt = "Craft a tale about a valiant warrior on a quest to uncover priceless treasures hidden within a mystical island."
+    )
+    llm.address_request(agent_process)
+    assert isinstance(agent_process.get_response(), str)
+    # print(response)
+
+def test_open_llm():
+    llm_type = "mistral-7b-it"
+    max_gpu_memory = {"2": "48GB"}
+    eval_device = "cuda:2"
+    max_new_tokens = 256
+    llm = LLMKernel(
+        llm_type,
+        max_gpu_memory=max_gpu_memory,
+        eval_device=eval_device,
+        max_new_tokens=max_new_tokens
+    )
+    agent_process = AgentProcess(
+        agent_name = "Narrative Agent",
+        prompt = "Craft a tale about a valiant warrior on a quest to uncover priceless treasures hidden within a mystical island."
+    )
+    context_manager = SimpleContextManager()
+    agent_process.set_pid(0)
+    agent_process.set_time_limit(1)
+    llm.address_request(agent_process)
+    assert context_manager.check_restoration(0) is True
+    context_manager.clear_restoration(0)
