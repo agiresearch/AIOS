28f64fa2cf4f64149b6c4d32f63f4edf1ad31b50
Jackson Davis <65826384+itsthemoon@users.noreply.github.com>
2024-05-14 19:41:16 -0400

    feat: Add gpt-4o model to model registry

--- src/llm_kernel/llm_classes/model_registry.py
+++ src/llm_kernel/llm_classes/model_registry.py
@@ -8,5 +8,6 @@ MODEL_REGISTRY = {
     'bedrock/anthropic.claude-3-haiku-20240307-v1:0': BedrockLLM,
     'gemini-pro': GeminiLLM,
     'gpt-3.5-turbo': GPTLLM,
-    'gpt-4': GPTLLM
+    'gpt-4': GPTLLM,
+    'gpt-4o': GPTLLM
 }
