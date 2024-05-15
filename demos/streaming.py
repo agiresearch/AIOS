963761ff1ab2cc0c834cc18dc3469971aa02bb32
BRama10 <grand.hunter.dark.15@gmail.com>
2024-05-15 15:52:46 -0400

    remove personal info

--- demos/streaming.py
+++ demos/streaming.py
@@ -1,9 +1,3 @@
-# bottom two lines fix src import error 
-# replace path with your path to AIOS
-
-# import sys, os
-# sys.path.append('/Users/rama2r/AIOS')
-
 from src.custom_kernels.kernels import GPTKernel
 
 g = GPTKernel()
@@ -15,11 +9,10 @@ import time
 
 
 time.sleep(2) 
-# Now let's pause for 2 seconds
+
 print("Pausing...")
 g.pause()
 time.sleep(2)  # Simulate some processing time
 
-# Let's play for 3 seconds
 print("Playing...")
 g.play()
