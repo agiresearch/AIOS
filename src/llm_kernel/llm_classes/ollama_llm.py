636957db3245871b3d71f8d1bbab9a2666d86c4f
Kai Mei <kai.mei@rutgers.edu>
2024-06-21 20:19:09 -0700

    feat: add function calling for open-sourced models (native huggingface, vllm, ollama) (#144)

--- src/llm_kernel/llm_classes/ollama_llm.py
+++ src/llm_kernel/llm_classes/ollama_llm.py
@@ -8,6 +8,8 @@ import ollama
 from transformers import AutoTokenizer
 
 from pyopenagi.utils.chat_template import Response
+
+import json
 class OllamaLLM(BaseLLMKernel):
 
     def __init__(self, llm_name: str,
@@ -38,19 +40,56 @@ class OllamaLLM(BaseLLMKernel):
         agent_process.set_status("executing")
         agent_process.set_start_time(time.time())
         messages = agent_process.query.messages
+        tools = agent_process.query.tools
         self.logger.log(
             f"{agent_process.agent_name} is switched to executing.\n",
             level = "executing"
         )
-        response = ollama.chat(
-            model=self.model_name.split("/")[-1],
-            messages=messages
-        )
-        agent_process.set_response(
-            Response(
-                response_message = response['message']['content']
+
+        # print(f"Current messages: {messages}, use tools: {True if tools else False}")
+
+        if tools:
+            messages = self.tool_calling_input_format(messages, tools)
+            response = ollama.chat(
+                model=self.model_name.split("/")[-1],
+                messages=messages
             )
-        )
+
+            # print(response)
+            tool_calls = self.tool_calling_output_format(
+                response["message"]["content"]
+            )
+
+            if tool_calls:
+                # print("Match json format")
+                # print(f"Matched tool call is: {tool_calls}")
+
+                agent_process.set_response(
+                    Response(
+                        response_message = None,
+                        tool_calls = tool_calls
+                    )
+                )
+            else:
+                agent_process.set_response(
+                    Response(
+                        response_message = response['message']['content']
+                    )
+                )
+
+        else:
+            messages = self.tool_calling_input_format(messages, tools)
+            response = ollama.chat(
+                model=self.model_name.split("/")[-1],
+                messages=messages
+            )
+            # print(response)
+            agent_process.set_response(
+                Response(
+                    response_message = response['message']['content']
+                )
+            )
+
         agent_process.set_status("done")
         agent_process.set_end_time(time.time())
         return
