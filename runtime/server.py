0737541fdede8000e2ffbcae6d621590b9d0edd1
BRama10 <grand.hunter.dark.15@gmail.com>
2024-07-09 15:42:25 -0400

    feat: created AIOS runtime template (#164)

---
+++ runtime/server.py
@@ -0,0 +1,22 @@
+from starlette.requests import Request
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/run_agent")
+async def run_agent(*args, **kwargs):
+    pass
+
+@app.get("/get_all_agents")
+async def get_all_agents(*args, **kwargs):
+    pass
+
