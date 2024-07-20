1366bc8f676996fd5977029a554d89a098549859
BRama10 <grand.hunter.dark.15@gmail.com>
2024-07-20 16:59:16 -0400

    perf: remove excess llm cores (#178)
    * docs: formatting issue for test logs display
    
    * perf: removed deprecated gemini & openai models

--- aios/llm_core/llm_classes/model_registry.py
+++ aios/llm_core/llm_classes/model_registry.py
@@ -7,11 +7,6 @@ from .claude_llm import ClaudeLLM
 
 #used for closed LLM model registry
 MODEL_REGISTRY = {
-    'bedrock/anthropic.claude-3-haiku-20240307-v1:0': BedrockLLM,
-    # Gemini-1.0
-    'gemini-1.0-pro': GeminiLLM,
-    "gemini-1.0-pro-001": GeminiLLM,
-
     # Gemini-1.5
     "gemini-1.5-flash": GeminiLLM,
     "gemini-1.5-pro": GeminiLLM,
@@ -20,17 +15,12 @@ MODEL_REGISTRY = {
     'gpt-3.5-turbo': GPTLLM,
     'gpt-4-turbo': GPTLLM,
 
-    # GPT4
-    'gpt-4-turbo-2024-04-09': GPTLLM,
-    'gpt-4-turbo-preview': GPTLLM,
-    'gpt-4-0125-preview': GPTLLM,
-    'gpt-4': GPTLLM,
-
     # GPT4o
     'gpt-4o': GPTLLM,
     'gpt-4o-2024-05-13': GPTLLM,
     'gpt-4o-mini': GPTLLM,
 
     # claude
-    'claude-3-5-sonnet-20240620': ClaudeLLM
+    'claude-3-5-sonnet-20240620': ClaudeLLM,
+    'bedrock/anthropic.claude-3-haiku-20240307-v1:0': BedrockLLM
 }
