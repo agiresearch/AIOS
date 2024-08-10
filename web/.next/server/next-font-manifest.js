4022517b3360ba3ae47e6fd884d98e540e5a570c
BRama10 <grand.hunter.dark.15@gmail.com>
2024-08-10 12:39:51 -0400

    bug: fresh exclude of cache

--- web/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/rama2r/AIOS/web/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WwWDd0p44sJbFdkITKrxdtgYH+HUrPjhlENIy5CyiHA=\"\n}"
