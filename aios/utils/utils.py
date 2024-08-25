9d183e841014cb87d0c805401f72e8a0ef0bc6f5
Kai Mei <kai.mei@rutgers.edu>
2024-08-25 15:30:20 -0700

    remove sleep of gpt llm (#218)

--- aios/utils/utils.py
+++ aios/utils/utils.py
@@ -23,6 +23,8 @@ def parse_global_args():
     parser.add_argument('--max_gpu_memory', type=json.loads, help="Max gpu memory allocated for the LLM")
     parser.add_argument('--eval_device', type=str, help="Evaluation device")
     parser.add_argument('--max_new_tokens', type=int, default=256, help="The maximum number of new tokens for generation")
+    parser.add_argument("--agent_log_mode", type=str,default="console",choices=["console", "file"])
+    parser.add_argument("--scheduler_log_mode", type=str,default="console",choices=["console", "file"])
     parser.add_argument("--llm_kernel_log_mode", type=str, default="console", choices=["console", "file"])
     parser.add_argument("--use_backend", type=str, default="ollama", choices=["ollama", "vllm"])
 
