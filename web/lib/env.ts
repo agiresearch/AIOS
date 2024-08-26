ad9143cf122199c7567debfd90eecd0598947629
Balaji R <grand.hunter.dark.15@gmail.com>
2024-08-26 01:07:26 -0400

    feat: faster inference times

--- web/lib/env.ts
+++ web/lib/env.ts
@@ -1,3 +1,3 @@
 export const inDevEnvironment = !!process && process.env.NODE_ENV === 'development';
-// export const serverUrl = inDevEnvironment ? 'http://localhost:8000' : 'https://myapp-y5z35kuonq-uk.a.run.app'
-export const serverUrl = 'https://myapp-y5z35kuonq-uk.a.run.app';
+export const serverUrl = inDevEnvironment ? 'http://localhost:8000' : 'https://myapp-y5z35kuonq-uk.a.run.app'
+// export const serverUrl = 'https://myapp-y5z35kuonq-uk.a.run.app';
