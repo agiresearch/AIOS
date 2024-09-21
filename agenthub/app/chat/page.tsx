39946842cff92486c45ec4d88c0e8d06652f0738
BRama10 <grand.hunter.dark.15@gmail.com>
2024-09-21 15:09:10 -0400

    chat

--- agenthub/app/chat/page.tsx
+++ agenthub/app/chat/page.tsx
@@ -6,13 +6,14 @@ import { Header } from "@/components/chat/header";
 
 const Chat = () => {
     return (
-        <div className="bg-neutral-800 w-full h-full flex flex-col items-center">
-            <Header />
-            <div className="flex flex-col h-[85vh] w-full">
+        <div className="bg-neutral-800 w-full h-[85vh] flex flex-col items-center">
+            {/* <Header /> */}
+            <div className='h-[40px] w-full'></div>
+            <div className="flex flex-col h-full w-3/5 items-center relative">
                 <Body />
                 <div className="w-full fixed bottom-0">
                     <Form />
-                    <p className="w-full text-center text-xs text-neutral-400 my-2 lg:pr-[300px]">TalkGPT could make errors. Consider checking important information.</p>
+                    <p className="w-full text-center text-xs text-neutral-400 py-2 lg:pr-[300px] bg-neutral-800 ">TalkGPT could make errors. Consider checking important information.</p>
                 </div>
             </div>
         </div>
--- agenthub/components/chat/body/message-box.tsx
+++ agenthub/components/chat/body/message-box.tsx
@@ -18,14 +18,14 @@ export const MessageBox = ({
         <div
             className="flex space-x-3 items-start mb-10 max-w-[calc(80%)] md:max-w-full text-wrap"
         >
-            <Avatar className="w-7 h-7 text-white fill-white">
+            <Avatar className="w-10 h-10 text-white fill-white">
                 <AvatarImage src={imageUrl} className="text-white fill-white" />
                 <AvatarFallback className="text-neutral-900 font-semibold">
                     {nameString[0]}
                 </AvatarFallback>
             </Avatar>
-            <div className="max-w-[calc(80%)]">
-                <h3 className="font-bold text-white">{nameString}</h3>
+            <div className="max-w-full w-full">
+                <h3 className="font-bold text-white text-xl">{nameString}</h3>
                 <div className="flex flex-grow flex-col gap-3 gap-y-5 text-white">
                     <Markdown content={message.content} />
                 </div>
