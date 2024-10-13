f4a91874e9fbdc14f1dd7e5246c35df33660a1c8
BRama10 <grand.hunter.dark.15@gmail.com>
2024-10-13 16:20:48 -0400

    refactor: modularize

--- agenthub/interfaces/agentchat/index.ts
+++ agenthub/interfaces/agentchat/index.ts
@@ -5,29 +5,6 @@ export interface Message {
     sender: 'user' | 'bot';
     timestamp: Date;
     attachments?: string[]; // Array of file names or URLs
-  }
-export interface HeaderProps {
-    darkMode: boolean;
-    setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-export interface MessageBubbleProps {
-    message: Message;
-    darkMode: boolean;
-    index: number;
-    isThinking?: boolean;
-  }
-
-export interface MessageListProps {
-    messages: Message[];
-    darkMode: boolean;
-}
-
-export interface InputAreaProps {
-    input: string;
-    setInput: React.Dispatch<React.SetStateAction<string>>;
-    handleSend: () => void;
-    darkMode: boolean;
 }
 
 export interface Chat {
@@ -35,11 +12,3 @@ export interface Chat {
     name: string;
 }
 
-export interface SidebarProps {
-    chats: Chat[];
-    activeChat: number;
-    setActiveChat: (id: number) => void;
-    addChat: () => void;
-    updateChatName: (chatId: number, newName: string) => void;
-    darkMode: boolean;
-}
