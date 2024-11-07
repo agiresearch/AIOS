4ce24a4aae437f4d1b7dbd76710ecd58691db6ff
xiang <xz0224@bu.edu>
2024-11-06 21:08:53 -0500

    feat: integrate SeeAct with AIOS LLM service

--- pyopenagi/agents/agent_factory.py
+++ pyopenagi/agents/agent_factory.py
@@ -5,9 +5,29 @@ from .interact import Interactor
 from ..manager.manager import AgentManager
 import os
 import importlib
+
+
 class AgentFactory:
-    def __init__(self, agent_log_mode):
+    def __init__(self,
+                #  agent_process_queue,
+                #  agent_process_factory,
+                 agent_log_mode
+        ):
+        # self.max_aid = 256
+        # # self.llm = llm
+        # self.aid_pool = [i for i in range(self.max_aid)]
+        # heapq.heapify(self.aid_pool)
+        # self.agent_process_queue = agent_process_queue
+        # self.agent_process_factory = agent_process_factory
+
+        # self.current_agents = {}
+
+        # self.current_agents_lock = Lock()
+
+        # self.terminate_signal = Event()
+
         self.agent_log_mode = agent_log_mode
+
         self.manager = AgentManager('https://my.aios.foundation/')
 
     def snake_to_camel(self, snake_str):
@@ -16,6 +36,7 @@ class AgentFactory:
 
     def list_agents(self):
         agent_list = self.manager.list_available_agents()
+        
         for agent in agent_list:
             print(agent)
 
@@ -33,6 +54,18 @@ class AgentFactory:
             return self.manager.load_agent(*name_split)
 
     def activate_agent(self, agent_name: str, task_input):
+        # script_path = os.path.abspath(__file__)
+        # script_dir = os.path.dirname(script_path)
+
+        # downloads the agent if its not installed already
+        # interactor = Interactor()
+
+        # if not os.path.exists(os.path.join(script_dir, agent_name)):
+        #     interactor.download_agent(agent_name)
+
+        # if not interactor.check_reqs_installed(agent_name):
+        #     interactor.install_agent_reqs(agent_name)
+
         # First try local loading
         try:
             agent_class = self.load_agent_instance(agent_name)
@@ -51,8 +84,20 @@ class AgentFactory:
         agent = agent_class(
             agent_name=agent_name,
             task_input=task_input,
+            # agent_process_factory = self.agent_process_factory,
             log_mode=self.agent_log_mode
         )
+
+
+        # set the identifier for the agent
+        # aid = heapq.heappop(self.aid_pool)
+        # agent.set_aid(aid)
+
+        # # use a lock to make sure only one agent can read the values at a time
+        # if not self.terminate_signal.is_set():
+        #     with self.current_agents_lock:
+        #         self.current_agents[aid] = agent
+
         return agent
 
     def run_agent(self, agent_name, task_input):
@@ -60,7 +105,9 @@ class AgentFactory:
             agent_name=agent_name,
             task_input=task_input
         )
+        # print(task_input)
         output = agent.run()
+        # self.deactivate_agent(agent.get_aid())
         return output
 
     def print_agent(self):
@@ -76,7 +123,9 @@ class AgentFactory:
             )
         self.print(headers=headers, data=data)
 
+
     def print(self, headers, data):
+        # align output
         column_widths = [
             max(len(str(row[i])) for row in [headers] + data) for i in range(len(headers))
         ]
@@ -94,4 +143,6 @@ class AgentFactory:
         return row_str
 
     def deactivate_agent(self, aid):
+        # self.current_agents.pop(aid)
+        # heapq.heappush(self.aid_pool, aid)
         pass
