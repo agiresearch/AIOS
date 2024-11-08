7a8c9d9bdcfdfd89bde05613b84f267079199b84
2020-qqtcg <mj1921761583@gmail.com>
2024-11-08 13:35:06 +0800

    feat: standard

--- experiment/benchmark/gaia/run_infer.py
+++ experiment/benchmark/gaia/run_infer.py
@@ -15,9 +15,21 @@ DATA_PATH = os.path.join(
     "validation"
 )
 
+SYSTEM_PROMPT = """You are a general AI assistant. I will ask you a question. Report your thoughts, and finish
+your answer with the following template: FINAL ANSWER: [YOUR FINAL ANSWER].
+YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of
+numbers and/or strings.
+If you are asked for a number, don’t use comma to write your number neither use units such as $ or percent
+sign unless specified otherwise.
+If you are asked for a string, don’t use articles, neither abbreviations (e.g. for cities), and write the digits in
+plain text unless specified otherwise.
+If you are asked for a comma separated list, apply the above rules depending of whether the element to be put
+in the list is a number or a string."""
+
 
 def process_one_func(data):
     agent = StandardAgent("Standard Agent", data["Question"])
+    agent.custom_prompt = lambda: SYSTEM_PROMPT
     result = agent.run()
     print(result)
     return {
