7dbd50359f7cc3a9fd878aba13176abac717a348
Joshua Park <123624726+joshprk@users.noreply.github.com>
2024-11-20 19:53:10 -0500

    fix: use locally hosted kernel in development environments

--- agenthub/lib/env.ts
+++ agenthub/lib/env.ts
@@ -2,5 +2,5 @@ export const inDevEnvironment = !!process && process.env.NODE_ENV === 'developme
 // export const serverUrl = inDevEnvironment ? 'http://localhost:8000' : 'https://myapp-y5z35kuonq-uk.a.run.app'
 export const baseUrl = inDevEnvironment ? 'http://localhost:3000' : 'https://my.aios.foundation'
 // export const serverUrl = inDevEnvironment ? 'http://localhost:8000' : 'http://35.232.56.61:8000'
-export const serverUrl = 'http://35.232.56.61:8000';
+export const serverUrl = inDevEnvironment ? 'http://localhost:8000' : 'http://35.232.56.61:8000';
 
