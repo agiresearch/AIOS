335008c6aec64ff48f8bdfce5494752f6e426229
dongyuanjushi <kai.mei@rutgers.edu>
2024-11-25 06:10:37 -0500

    remove redundant files

--- cerebrum/memory/layer.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-
-@dataclass
-class MemoryLayer:
-    memory_limit: int = 104857600  # 100MB
-    eviction_k: int = 10
-    custom_eviction_policy: Optional[str] = None
